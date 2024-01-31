6f13c51f452954e545ff8b502b18a4f90c8bd07d
RuneDn <runedeneve@hotmail.be>
2024-01-31 19:57:45 +0100

    added contact info

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
 
-st.title('Me', anchor=False)
+st.markdown(
+            """
+            This is a web application that displays an overview of your trading 212 portfolio. 
+            It can be used to track your dividend history/progress and to get a complete overview of 
+            all positions held in the portfolio and their (unrealized) gain/loss.
+            
+            Created by Rune De Neve.
+            
+            (Student Business Engineering: Data Analytics at Ghent University)
+
+            ---
+            Contact me:
+
+            LinkedIn: [Rune De Neve](https://www.linkedin.com/in/rune-de-neve)
+            
+            E-mail: runedeneve@hotmail.be
+            """
+        )
